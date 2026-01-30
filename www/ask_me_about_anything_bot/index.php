c1afca70048020ccef43e34d5e8710ef724482cd
MihailStar <mhlstr@mail.ru>
2026-01-30 23:10:15 +0300

    refactor

--- www/ask_me_about_anything_bot/index.php
+++ www/ask_me_about_anything_bot/index.php
@@ -3,68 +3,63 @@
 declare(strict_types=1);
 
 /** @var string|false */
-$secretToken = getenv("SECRET_TOKEN");
-if ($secretToken === false) {
-    http_response_code(500);
-    exit();
-}
-
-/** @var string|false */
 $botToken = getenv("BOT_TOKEN");
 if ($botToken === false) {
     http_response_code(500);
-    exit();
+    exit(1);
 }
 
 /** @var string */
-define("API_URL", "https://api.telegram.org/bot" . $botToken);
+define("API_URL", "api.telegram.org/bot" . $botToken);
+
+/** @var string|false */
+$secretToken = getenv("SECRET_TOKEN");
+if ($secretToken === false) {
+    http_response_code(500);
+    exit(1);
+}
 
 /** @var string|null */
-$incomingSecretToken = $_SERVER['HTTP_X_TELEGRAM_BOT_API_SECRET_TOKEN'] ?? null;
+$incomingSecretToken = $_SERVER["HTTP_X_TELEGRAM_BOT_API_SECRET_TOKEN"] ?? null;
 if ($incomingSecretToken === null || !hash_equals($secretToken, $incomingSecretToken)) {
     http_response_code(403);
     exit();
 }
 
-/** @var string|false */
 $rawBody = file_get_contents("php://input");
 if ($rawBody === false) {
     http_response_code(400);
     exit();
 }
 
-/** @var array|null */
+/**
+ * @var array{message?:array{chat:array{id:int},text?:string}}|null
+ * @tutorial https://core.telegram.org/bots/api#update
+ */
 $update = json_decode($rawBody, true);
 if ($update === null) {
     http_response_code(400);
     exit();
 }
 
-/** @var int */
 $chatId = $update["message"]["chat"]["id"];
 
-/** @var string|null */
 $chatText = $update["message"]["text"] ?? null;
 if ($chatText === null) {
     sendMessage($chatId, "Напишите вопрос - получите ответ");
     exit();
 }
 
-switch ($chatText) {
-    case "/start":
-        sendMessage(
-            $chatId,
-            "Спросите меня о чем угодно" . PHP_EOL .
-                "Напишите вопрос - получите ответ"
-        );
-        break;
-
-    default:
-        sendMessage($chatId, "Вас это еб*ть не должно");
-        break;
+if ($chatText === "/start") {
+    sendMessage(
+        $chatId,
+        "Спросите меня о чем угодно" . PHP_EOL .
+            "Напишите вопрос - получите ответ"
+    );
+} else {
+    sendMessage($chatId, "Вас это еб*ть не должно");
 }
 
-/** @return string|false */
 function sendMessage(int $chatId, string $text)
 {
     $queryParameters = http_build_query([
